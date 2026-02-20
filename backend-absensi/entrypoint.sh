8d99022adc5e2f814acfe0301912803fa74ca370
radityaWahyu <radityaw@gmail.com>
2026-02-20 07:32:09 +0700

    delete docker file

--- backend-absensi/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Menjalankan migrasi database (opsional, hati-hati di production)
-# php artisan migrate --force
-
-# Menjalankan Laravel Octane dengan FrankenPHP
-php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000
